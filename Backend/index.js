1d03eb0c227b407b1849b903f08dc89f49e3f441
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-22 19:59:20 +0530

    wrote whole backend

--- Backend/index.js
+++ Backend/index.js
@@ -2,29 +2,59 @@
 const express = require("express");
 const { createTodo } = require("./types");
 const { updateTodo} = require("./types");
+const { todo } = require("./db");
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo",function(req, res){
+app.post("/todo", function(req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
-    if(!parsedPayload.success) {
+
+    if (!parsedPayload.success) {
         res.status(411).json({
             msg: "You sent the wrong inputs"
-        })
+        });
         return;
     }
-    // putting in mongodb
 
+    // Putting in MongoDB using .then
+    todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+    })
+    .then((createdTodo) => {
+        res.status(200).json({
+            msg: "Todo created successfully",
+            data: createdTodo
+        });
+    })
+    .catch(function(error) {
+        res.status(500).json({
+            msg: "There was an error creating the todo",
+            error: error.message
+        });
+    });
+});
 
-})
 
 app.get("/todos",function(req, res){
-
-
+    todo.find({})
+    .then(function(todo){
+        res.status(200).json({
+            msg: "Your todos are:",
+            todo: todo
+        })
+    })
+    .catch(function(error){
+        res.status(500).json({
+            msg: "There was a problem in fetching the todos",
+            error: error.message
+        })
+    })
 })
 
+// update a specific todo
 app.put("/completed",function(req, res){
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
@@ -35,5 +65,21 @@ app.put("/completed",function(req, res){
         return;
     }
 
+    todo.update({
+        _id: req.body.id
+    },{
+        completed: true
+    })
 
+    .then(function(todo){
+        res.status(200).json({
+            msg: "Your todo is successfully updated"
+        })
+    })
+    .catch(function(error){
+        res.status(500).json({
+            msg: "There was a problem in updating the todos",
+            error: error.message
+        })
+    })
 })
