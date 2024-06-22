ae8d3f68b70679e18df5cbfc0df2148a9375c022
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-22 23:31:47 +0530

    added state variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,12 @@ import { Create_Todo } from './components/Create_Todo'
 import { Todos } from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState(0)
 
   return (
     <div>
       <Create_Todo></Create_Todo>
-      <Todos></Todos>
+      <Todos todos = {}></Todos>
     </div>
   )
 }
