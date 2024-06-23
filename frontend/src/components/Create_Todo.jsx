d855aafc19c7b7ac14953d5467063f9287bfc440
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-06-23 15:29:14 +0530

    added function events to both the placeholders

--- frontend/src/components/Create_Todo.jsx
+++ frontend/src/components/Create_Todo.jsx
@@ -1,23 +1,38 @@
 
 export function Create_Todo(){
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("")
     return <div>
         <input style = {{
             padding: 10,
             margin: 10
         }}
-        type = "text" placeholder = "title"></input><br></br>
+        type = "text" placeholder = "title" onChange={function(event){
+            const value = event.target.value;  // event.target targets to this title DOM
+            setTitle(value);
+        }}></input><br></br>
 
         <input style = {{
             padding: 10,
             margin: 10
         }}
-        type = "text" placeholder = "description"></input><br></br>
+        type = "text" placeholder = "description" onChange={function(event){
+            const value = event.target.value;
+            setDescription(value);
+        }}></input><br></br>
 
         <button style = {{
             padding: 10,
             margin: 10
         }} onClick = {function(){
-            fetch("http://localhost:3000/todos")
+            fetch("http://localhost:3000/todos"),{
+                method: "POST",
+                body:{
+                    title: "title",
+                    description: "description",
+                }
+            }
             .then(async function(response){
                 const json = await response.json();
                 alert("To do added successfully")
